d2c8d6c93c2a94a5170ab4d3acf890af8d14fc0d
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-08-05 23:50:49 +0900

    デザインを改善

--- dist/browser_action.js
+++ dist/browser_action.js
@@ -9,10 +9,20 @@
 /***/ (() => {
 
 
+const selector_of_selected_item = ".tab_switcher_item[data-selected]";
+const default_selected_item = document.querySelector(selector_of_selected_item);
+if (default_selected_item) {
+    const target_selector = default_selected_item.dataset.target;
+    if (target_selector) {
+        const target = document.querySelector(target_selector);
+        if (target)
+            target.style.display = "block";
+    }
+}
 const tab_switcher_item = document.querySelectorAll(".tab_switcher_item");
 tab_switcher_item.forEach((item) => {
     item.addEventListener("click", () => {
-        const selected_item = document.querySelector(".tab_switcher_item[data-selected]");
+        const selected_item = document.querySelector(selector_of_selected_item);
         if (selected_item)
             delete selected_item.dataset.selected;
         item.dataset.selected = "";
