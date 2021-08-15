319df49fcc25bcfdb062982962be114fcca118e2
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-08-16 00:20:48 +0900

    詳細設定ページのデザイン改善 #117 (#175)

--- dist/advanced_setting.js
+++ dist/advanced_setting.js
@@ -2,6 +2,100 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/advanced_setting_view.ts":
+/*!**************************************!*\
+  !*** ./src/advanced_setting_view.ts ***!
+  \**************************************/
+/***/ (() => {
+
+
+function adjust_scroll_position() {
+    const header = document.querySelector("header");
+    if (header)
+        scrollBy(0, parseInt(getComputedStyle(header).height.replace(/px$/, "")) * -2);
+    else
+        console.error("header was not found.");
+}
+class Menu {
+    constructor() {
+        this.overlay = document.getElementById("overlay");
+        this.menu = document.getElementById("menu");
+        this.item_outer = document.getElementById("menu_item_outer");
+        this.open_button = document.getElementById("menu_open_button");
+        this.close_button = document.getElementById("menu_close_button");
+        this.header = document.querySelector("header");
+        this.open_attribute = "data-open";
+        if (this.open_button) {
+            this.open_button.addEventListener("click", () => {
+                this.show();
+            });
+        }
+        else {
+            console.error("#menu_open_button was not found.");
+        }
+        if (this.close_button) {
+            this.close_button.addEventListener("click", () => {
+                this.hide();
+            });
+        }
+        else {
+            console.error("#menu_close_button was not found.");
+        }
+        if (this.overlay) {
+            this.overlay.addEventListener("click", () => {
+                this.hide();
+            });
+        }
+        else {
+            console.error("#overlay was not found.");
+        }
+        [...document.querySelectorAll("h2")].reverse().forEach((element) => {
+            if (getComputedStyle(element).display !== "none" && element.textContent && this.item_outer) {
+                const menu_item = document.createElement("div");
+                menu_item.className = "menu_item";
+                menu_item.textContent = element.textContent;
+                menu_item.addEventListener("click", () => {
+                    location.hash = element.id;
+                    adjust_scroll_position();
+                });
+                this.item_outer.insertAdjacentElement("afterbegin", menu_item);
+            }
+        });
+    }
+    show() {
+        if (!this.menu) {
+            console.error("#menu was not found.");
+            return;
+        }
+        else {
+            this.menu.setAttribute(this.open_attribute, "");
+            if (this.overlay)
+                this.overlay.style.display = "block";
+            else
+                console.error("#overlay was not found.");
+        }
+    }
+    hide() {
+        if (!this.menu) {
+            console.error("#menu was not found.");
+            return;
+        }
+        else {
+            this.menu.removeAttribute(this.open_attribute);
+            if (this.overlay)
+                this.overlay.style.display = "none";
+            else
+                console.error("#overlay was not found.");
+        }
+    }
+}
+new Menu();
+if (location.hash)
+    adjust_scroll_position();
+
+
+/***/ }),
+
 /***/ "./src/color.ts":
 /*!**********************!*\
   !*** ./src/color.ts ***!
@@ -181,6 +275,18 @@ class Setting {
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -219,6 +325,9 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _color__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./color */ "./src/color.ts");
 /* harmony import */ var _setting__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./setting */ "./src/setting.ts");
+/* harmony import */ var _advanced_setting_view__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./advanced_setting_view */ "./src/advanced_setting_view.ts");
+/* harmony import */ var _advanced_setting_view__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_advanced_setting_view__WEBPACK_IMPORTED_MODULE_2__);
+
 
 
 function get_setting_name(element) {
