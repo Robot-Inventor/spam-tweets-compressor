54e7169609f6c92d19e8fff8a20641e6d3811270
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-08-25 00:47:01 +0900

    URLの正規表現のピリオドがエスケープされていない #259 (#261)

--- src/normalize.ts
+++ src/normalize.ts
@@ -13,7 +13,7 @@ export function normalize(text: string): string {
 export function normalize_link(text: string): string {
     return text
         .replace(/^https?:\/\/(www\.)?/i, "")
-        .replace(/\/(index.html)?$/, "")
+        .replace(/\/(index\.html)?$/, "")
         .replace(/^.+?(\/|$)/, (s) => s.toLowerCase());
 }
 
