80a343ad6b535baba7462eecb453259bd81e88cd
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-08-26 18:48:14 +0900

    詳細設定ページから個人情報に関する説明を読めるようにする #267 (#272)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,24 @@
-const target_element: NodeListOf<HTMLElement> = document.querySelectorAll("*[data-i18n]");
-target_element.forEach((target) => {
-    const message_name = target.dataset.i18n;
-    if (!message_name) return;
+function init_i18n_text() {
+    const target_element: NodeListOf<HTMLElement> = document.querySelectorAll("*[data-i18n]");
+    target_element.forEach((target) => {
+        const message_name = target.dataset.i18n;
+        if (!message_name) return;
 
-    const message: string = browser.i18n.getMessage(message_name);
-    if (message) target.textContent = message;
-});
+        const message: string = browser.i18n.getMessage(message_name);
+        if (message) target.textContent = message;
+    });
+}
+
+function init_i18n_link() {
+    const target_element: NodeListOf<HTMLAnchorElement> = document.querySelectorAll("a[data-i18n-link]");
+    target_element.forEach((target) => {
+        const link_name = target.dataset.i18nLink;
+        if (!link_name) return;
+
+        const link: string = browser.i18n.getMessage(link_name);
+        if (link) target.href = link;
+    });
+}
+
+init_i18n_text();
+init_i18n_link();
