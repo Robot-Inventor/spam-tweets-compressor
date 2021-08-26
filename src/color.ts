e43ca4c7f6bbce58e5be006127b11b15d1e8d019
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-08-27 00:18:46 +0900

    Twitterの配色の取得に失敗することがあるバグ #278

--- src/color.ts
+++ src/color.ts
@@ -1,20 +1,34 @@
 import { selector } from "./selector";
 import { Setting } from "./setting";
 
-export async function update_color_setting(): Promise<void> {
+export async function update_color_setting(retry = true): Promise<void> {
     const setting = await new Setting().load();
+
+    let success = true;
 
     const tweet_button_inner = document.querySelector(selector.tweet_button_inner);
     if (tweet_button_inner) {
         const main_color = getComputedStyle(tweet_button_inner).backgroundColor;
         if (main_color) setting.main_color = main_color;
-    }
+    } else success = false;
 
     const background_color = getComputedStyle(document.body).backgroundColor;
     if (background_color) setting.background_color = background_color;
+    else success = false;
 
     const account_name = document.querySelector(selector.normal_text);
     if (account_name) setting.font_color = getComputedStyle(account_name).color;
+    else success = false;
+
+    if (!success && retry) {
+        return new Promise((resolve, reject) => {
+            setInterval(() => {
+                update_color_setting(false)
+                    .then(() => resolve())
+                    .catch((e) => reject(e));
+            }, 1000);
+        });
+    } else if (!success) throw "Failed to get color setting.";
 }
 
 function change_opacity(rgb: string, opacity: number) {
