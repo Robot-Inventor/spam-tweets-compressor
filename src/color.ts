3fc3e57e8a6824936617c2e8dddf213ba4711cf4
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-08-28 02:17:40 +0900

    Twitterの配色取得の再試行に失敗した場合の挙動を変更 #284 (#287)

--- src/color.ts
+++ src/color.ts
@@ -4,23 +4,17 @@ import { Setting } from "./setting";
 export async function update_color_setting(retry = true): Promise<void> {
     const setting = await new Setting().load();
 
-    let success = true;
-
     const tweet_button_inner = document.querySelector(selector.tweet_button_inner);
-    if (tweet_button_inner) {
-        const main_color = getComputedStyle(tweet_button_inner).backgroundColor;
-        if (main_color) setting.main_color = main_color;
-    } else success = false;
-
     const background_color = getComputedStyle(document.body).backgroundColor;
-    if (background_color) setting.background_color = background_color;
-    else success = false;
-
     const account_name = document.querySelector(selector.normal_text);
-    if (account_name) setting.font_color = getComputedStyle(account_name).color;
-    else success = false;
 
-    if (!success && retry) {
+    if (tweet_button_inner && background_color && account_name) {
+        setting.main_color = getComputedStyle(tweet_button_inner).backgroundColor;
+
+        setting.background_color = background_color;
+
+        setting.font_color = getComputedStyle(account_name).color;
+    } else if (retry) {
         return new Promise((resolve, reject) => {
             setInterval(() => {
                 update_color_setting(false)
@@ -28,7 +22,7 @@ export async function update_color_setting(retry = true): Promise<void> {
                     .catch((e) => reject(e));
             }, 1000);
         });
-    } else if (!success) throw "Failed to get color setting.";
+    } else throw "Failed to get color setting.";
 }
 
 function change_opacity(rgb: string, opacity: number) {
