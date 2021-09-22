3d30b134ac2a0c4aa272bd85602754792304f026
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-09-23 00:59:10 +0900

    詳細設定ページのメニューをmwc-drawerに置き換え #367 (#372)

--- src/advanced_setting_view.ts
+++ src/advanced_setting_view.ts
@@ -4,7 +4,7 @@ import { CheckListItem } from "@material/mwc-list/mwc-check-list-item";
  * Shift the scroll position by the height of the header.
  */
 const adjust_scroll_position = () => {
-    const header = document.querySelector("header");
+    const header = document.querySelector("mwc-top-app-bar-fixed");
     // eslint-disable-next-line radix, no-magic-numbers
     if (header) scrollBy(0, parseInt(getComputedStyle(header).height.replace(/px$/u, "")) * -2);
     else console.error("header was not found.");
@@ -41,30 +41,23 @@ const generate_check_list_item = (filter_name: string, filter_id: string, select
  * Initialize menu.
  */
 class Menu {
-    private readonly overlay: HTMLElement | null;
-    private readonly menu: HTMLElement | null;
-    private readonly item_outer: HTMLElement | null;
     private readonly open_button: HTMLElement | null;
+    private readonly item_outer: HTMLElement | null;
     private readonly close_button: HTMLElement | null;
-    private readonly open_attribute: string;
 
     constructor() {
-        this.overlay = document.getElementById("overlay");
-        this.menu = document.getElementById("menu");
-        this.item_outer = document.getElementById("menu_item_outer");
         this.open_button = document.getElementById("menu_open_button");
+        this.item_outer = document.getElementById("menu_item_outer");
         this.close_button = document.getElementById("menu_close_button");
-        this.open_attribute = "data-open";
-
-        if (this.open_button) this.open_button.addEventListener("click", () => this.show());
-        else console.error("#menu_open_button was not found.");
 
         if (this.close_button) this.close_button.addEventListener("click", () => this.hide());
         else console.error("#menu_close_button was not found.");
 
-        if (this.overlay) this.overlay.addEventListener("click", () => this.hide());
-        else console.error("#overlay was not found.");
+        this.init_menu_item();
+        Menu.init_menu_button();
+    }
 
+    private init_menu_item() {
         [...document.querySelectorAll("h2")].reverse().forEach((element) => {
             if (getComputedStyle(element).display !== "none" && element.textContent && this.item_outer) {
                 const menu_item = document.createElement("div");
@@ -79,22 +72,20 @@ class Menu {
         });
     }
 
-    private show() {
-        if (this.menu) {
-            this.menu.setAttribute(this.open_attribute, "");
-            if (this.overlay) this.overlay.style.display = "block";
-        } else {
-            console.error("#menu was not found.");
+    private static init_menu_button() {
+        const drawer = document.querySelector("mwc-drawer");
+        if (drawer) {
+            const container = drawer.parentNode;
+            if (container) {
+                container.addEventListener("MDCTopAppBar:nav", () => {
+                    drawer.open = !drawer.open;
+                });
+            }
         }
     }
 
     private hide() {
-        if (this.menu) {
-            this.menu.removeAttribute(this.open_attribute);
-            if (this.overlay) this.overlay.style.display = "none";
-        } else {
-            console.error("#menu was not found.");
-        }
+        if (this.open_button) this.open_button.click();
     }
 }
 
