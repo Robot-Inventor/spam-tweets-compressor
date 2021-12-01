cc5c191fd13d931e73999c1772b2e1e91972d8b0
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-12-01 23:18:23 +0900

    高度なフィルターの読み込み失敗時の処理を追加 #557 (#558)

--- src/ts/types/common/type_predicate_utility.test.ts
+++ src/ts/types/common/type_predicate_utility.test.ts
@@ -1,4 +1,4 @@
-import { is_object, is_string_array } from "./type_predicate_utility";
+import { is_error, is_object, is_string_array } from "./type_predicate_utility";
 
 test("Test of is_object()", () => {
     expect(is_object({})).toBe(true);
@@ -31,4 +31,20 @@ test("Test of is_string_array()", () => {
     // eslint-disable-next-line no-magic-numbers
     expect(is_string_array(1)).toBe(false);
     expect(is_string_array(true)).toBe(false);
+});
+
+test("Test of is_error()", () => {
+    expect(is_error(new Error())).toBe(true);
+    expect(is_error(new TypeError())).toBe(true);
+    expect(is_error(new SyntaxError())).toBe(true);
+    expect(is_error(new ReferenceError())).toBe(true);
+    expect(is_error(new RangeError())).toBe(true);
+    expect(is_error(new URIError())).toBe(true);
+    expect(is_error(new EvalError())).toBe(true);
+    expect(is_error(null)).toBe(false);
+    // eslint-disable-next-line no-undefined
+    expect(is_error(undefined)).toBe(false);
+    expect(is_error("aaa")).toBe(false);
+    // eslint-disable-next-line no-magic-numbers
+    expect(is_error(1)).toBe(false);
 });
--- src/ts/types/common/type_predicate_utility.ts
+++ src/ts/types/common/type_predicate_utility.ts
@@ -4,4 +4,6 @@ const is_object = (input: unknown): input is { [key: string]: unknown } =>
 const is_string_array = (input: unknown): input is Array<string> =>
     Array.isArray(input) && input.every((element) => typeof element === "string");
 
-export { is_object, is_string_array };
+const is_error = (input: unknown): input is Error => Object.prototype.toString.call(input) === "[object Error]";
+
+export { is_object, is_string_array, is_error };
