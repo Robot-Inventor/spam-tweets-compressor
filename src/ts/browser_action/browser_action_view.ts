cce6760596aa3faa945f7449d5445041c9cd22b7
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-12-27 14:15:17 +0900

    browser actionのタブ切り替えにトランジションを追加 #383 (#609)

--- src/ts/browser_action/browser_action_view.ts
+++ src/ts/browser_action/browser_action_view.ts
@@ -5,36 +5,93 @@ import { initialize_tooltip } from "./../common/tooltip";
  * Initialize tab switcher.
  */
 class TabSwitcher {
+    private old_tab_index: number;
+    private readonly active_item_attribute = "data-active-item";
+    private readonly tab_select_event = "MDCTabBar:activated";
+    private readonly selectors = {
+        setting_item_group: ".setting_item_group",
+        tab: "mwc-tab",
+        tab_bar: "mwc-tab-bar"
+    } as const;
+
     constructor() {
-        const tab_bar = document.querySelector("mwc-tab-bar");
+        this.old_tab_index = 0;
+
+        const tab_bar = document.querySelector(this.selectors.tab_bar);
         if (!tab_bar) {
-            console.error("mwc-tab-bar was not found.");
+            console.error(`${this.selectors.tab_bar} was not found.`);
             return;
         }
 
-        tab_bar.addEventListener("MDCTabBar:activated", (event: unknown) => {
+        tab_bar.addEventListener(this.tab_select_event, (event: unknown) => {
             const { detail } = event as { detail: { index: number } };
-            const item_index = detail.index;
-            const items = tab_bar.querySelectorAll("mwc-tab");
-            const selected_item = items[item_index];
+            const tab_index = detail.index;
+
+            // Selected tab was not changed.
+            if (tab_index === this.old_tab_index) return;
+
+            const tabs = tab_bar.querySelectorAll(this.selectors.tab);
+            const selected_item = tabs[tab_index];
+
+            const transition_direction = tab_index < this.old_tab_index ? "left" : "right";
+
+            // Set the transition for the old item.
+            const old_item = document.querySelector(`*[${this.active_item_attribute}]`);
+            if (old_item) {
+                old_item.removeAttribute(this.active_item_attribute);
+                old_item.classList.add(TabSwitcher.generate_transition_class_out(transition_direction));
+            }
 
             const tab_bar_transition_duration = 300;
             // eslint-disable-next-line no-magic-numbers
             const item_display_time_difference = tab_bar_transition_duration / 2;
             setTimeout(() => {
-                TabSwitcher.show_item(selected_item.dataset.target);
+                this.show_item(selected_item.dataset.target, transition_direction);
             }, item_display_time_difference);
+
+            this.old_tab_index = tab_index;
         });
+
+        // Show default item.
+        const first_tab = tab_bar.querySelector(this.selectors.tab);
+        if (first_tab) this.show_item(first_tab.dataset.target, "right");
     }
 
-    static show_item(selector: string | undefined) {
+    private show_item(selector: string | undefined, direction: "right" | "left") {
         if (!selector) return;
 
-        const item_group: NodeListOf<HTMLElement> = document.querySelectorAll(".setting_item_group");
-        item_group.forEach((element) => (element.style.display = "none"));
+        this.clear_all_transition_classes();
 
         const target: HTMLElement | null = document.querySelector(selector);
-        if (target) target.style.display = "block";
+        if (target) {
+            const active_items: NodeListOf<HTMLElement> = document.querySelectorAll(this.selectors.setting_item_group);
+            active_items.forEach((element) => {
+                element.style.display = "none";
+            });
+
+            target.classList.add(TabSwitcher.generate_transition_class_in(direction));
+            target.style.display = "block";
+
+            target.setAttribute(this.active_item_attribute, "");
+        }
+    }
+
+    private static generate_transition_class_in(direction: "right" | "left") {
+        return `change_to_${direction}_transition_in`;
+    }
+
+    private static generate_transition_class_out(direction: "right" | "left") {
+        return `change_to_${direction}_transition_out`;
+    }
+
+    private clear_all_transition_classes() {
+        const item_groups = document.querySelectorAll(this.selectors.setting_item_group);
+        item_groups.forEach((element) => {
+            for (const dir of ["right", "left"] as const) {
+                element.classList.remove(TabSwitcher.generate_transition_class_in(dir));
+                element.classList.remove(TabSwitcher.generate_transition_class_out(dir));
+            }
+        });
     }
 }
 
