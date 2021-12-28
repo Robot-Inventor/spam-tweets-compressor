458adadfb85084ecc2d010dbb0a82429849bd1d0
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-12-29 03:11:06 +0900

    設定のエクスポート時に名前をつけて保存できるようにする #610 (#618)

--- src/ts/advanced_setting/initialize/download_button.ts
+++ src/ts/advanced_setting/initialize/download_button.ts
@@ -1,4 +1,5 @@
 import { setting_object } from "../../types/common/setting";
+import { show_alert } from "../advanced_setting_view";
 
 /**
  * Copy specified text to clipboard.
@@ -36,12 +37,15 @@ const temporarily_change_button_text = (button: HTMLElement, text: string) => {
  */
 const initialize_copy_button = (setting: setting_object) => {
     const copy_button = document.getElementById("copy_button");
-    if (copy_button) {
-        copy_button.addEventListener("click", () => {
-            copy_text(convert_setting_to_json(setting));
-            temporarily_change_button_text(copy_button, browser.i18n.getMessage("advanced_setting_export_copied"));
-        });
-    } else console.error("Copy button was not found.");
+    if (!copy_button) {
+        console.error("Copy button was not found.");
+        return;
+    }
+
+    copy_button.addEventListener("click", () => {
+        copy_text(convert_setting_to_json(setting));
+        temporarily_change_button_text(copy_button, browser.i18n.getMessage("advanced_setting_export_copied"));
+    });
 };
 
 /**
@@ -49,14 +53,14 @@ const initialize_copy_button = (setting: setting_object) => {
  * @param json json string to download
  * @param file_name file name to download
  */
-const download_json = (json: string, file_name: string) => {
-    const download_link = document.createElement("a");
-    download_link.href = URL.createObjectURL(new Blob([json], { type: "text/json" }));
-    download_link.download = file_name;
-    download_link.style.display = "none";
-    document.body.appendChild(download_link);
-    download_link.click();
-    download_link.remove();
+const download_json = async (json: string, file_name: string) => {
+    const object_url = URL.createObjectURL(new Blob([json], { type: "text/json" }));
+
+    return browser.downloads.download({
+        filename: file_name,
+        saveAs: true,
+        url: object_url
+    });
 };
 
 /**
@@ -65,12 +69,38 @@ const download_json = (json: string, file_name: string) => {
  */
 const initialize_download_button = (setting: setting_object) => {
     const download_button = document.getElementById("save_button");
-    if (download_button) {
-        download_button.addEventListener("click", () => {
-            download_json(convert_setting_to_json(setting), "stc_setting.json");
-            temporarily_change_button_text(download_button, browser.i18n.getMessage("advanced_setting_export_saved"));
-        });
-    } else console.error("Download button was not found.");
+    if (!download_button) {
+        console.error("Download button was not found.");
+        return;
+    }
+
+    download_button.addEventListener("click", () => {
+        const json = convert_setting_to_json(setting);
+
+        const date_instance = new Date();
+        const year = date_instance.getFullYear();
+        /* eslint-disable no-magic-numbers */
+        const month = (date_instance.getMonth() + 1).toString().padStart(2, "0");
+        const date = date_instance.getDate().toString().padStart(2, "0");
+        const hour = date_instance.getHours().toString().padStart(2, "0");
+        const minute = date_instance.getMinutes().toString().padStart(2, "0");
+        /* eslint-enable no-magic-numbers */
+        const date_time = `${year}-${month}-${date}-${hour}-${minute}`;
+
+        const file_name = `settings_${date_time}.json`;
+
+        download_json(json, file_name)
+            .then(() =>
+                temporarily_change_button_text(
+                    download_button,
+                    browser.i18n.getMessage("advanced_setting_export_saved")
+                )
+            )
+            .catch((error) => {
+                console.error(error);
+                show_alert(browser.i18n.getMessage("advanced_setting_export_download_failed"));
+            });
+    });
 };
 
 const initialize_copy_and_download_button = (setting: setting_object) => {
