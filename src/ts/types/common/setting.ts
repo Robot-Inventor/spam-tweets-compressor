d680b0b31fc632a321fc313475ed8c087b14699d
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-12-29 15:54:05 +0900

    is_setting_object()関数をAjvを使ったバリデーターに置き換え #620 (#621)

--- src/ts/types/common/setting.ts
+++ src/ts/types/common/setting.ts
@@ -1,4 +1,3 @@
-import { is_object, is_string_array } from "./type_predicate_utility";
 import { ValidateFunction } from "ajv/dist/types/index";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore There is no type declaration because ./setting.validate is an automatically generated script.
@@ -29,61 +28,16 @@ interface setting_object {
     show_reason: boolean;
 }
 
-const is_color_setting = (input: unknown): input is ColorScheme => {
-    if (!is_object(input)) return false;
-
-    const all_properties: Array<keyof ColorScheme> = [
-        "main",
-        "main_light",
-        "background",
-        "background_light",
-        "high_emphasize_text",
-        "medium_emphasize_text",
-        "card",
-        "card_hover"
-    ];
-    const has_all_properties = all_properties.every(
-        (property) => property in input && typeof input[property] === "string"
-    );
-    return has_all_properties;
-};
-
-const is_setting_object = (input: unknown): input is setting_object => {
-    if (!is_object(input)) return false;
-
-    const all_properties: Array<keyof setting_object> = [
-        "advanced_filter",
-        "allow_list",
-        "color",
-        "decompress_on_hover",
-        "exclude_url",
-        "hide_completely",
-        "include_user_name",
-        "include_verified_account",
-        "ng_word",
-        "show_reason"
-    ];
-    const has_all_properties = all_properties.every((property) => property in input);
-    if (!has_all_properties) return false;
-
-    if (!is_string_array(input.advanced_filter)) return false;
-    if (!is_string_array(input.allow_list)) return false;
-    if (!is_color_setting(input.color)) return false;
-    if (typeof input.decompress_on_hover !== "boolean") return false;
-    if (!is_string_array(input.exclude_url)) return false;
-    if (typeof input.hide_completely !== "boolean") return false;
-    if (typeof input.include_user_name !== "boolean") return false;
-    if (typeof input.include_verified_account !== "boolean") return false;
-    if (!is_string_array(input.ng_word)) return false;
-    if (typeof input.show_reason !== "boolean") return false;
-
-    return true;
-};
-
 const get_setting_validator = () => {
     const validate = validate20 as ValidateFunction<setting_object>;
     return validate;
 };
 
+const is_setting_object = (input: unknown): input is setting_object => {
+    const validator = get_setting_validator();
+    const is_valid = validator(input);
+    return is_valid;
+};
+
 export { setting_object };
-export { is_setting_object, get_setting_validator };
+export { get_setting_validator, is_setting_object };
